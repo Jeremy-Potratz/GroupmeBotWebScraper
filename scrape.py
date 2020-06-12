a3f3410d51feae0b4a63a18386c56d477b2a52ad
Jeremy-Potratz <jpotrat@purdue.edu>
2020-06-11 22:56:55 -0500

    Adding nintendo scraper but running into a few problems. Done for the night

--- scrape.py
+++ scrape.py
@@ -1,6 +1,5 @@
 import requests
 import os
-from contextlib import closing
 from bs4 import BeautifulSoup
 from apscheduler.schedulers.blocking import BlockingScheduler
 
@@ -15,33 +14,61 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 #could connect to the spotify api to learn about new music i might like !!!
 # could look into my email possibly, idk if thats good though
 
+
+holder = ""
+counter = 0
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', hours=1)
+@sched.scheduled_job('interval', seconds=5)
 def main():
+    nintendo()
     grrm()
-    botw()
     return
 
-def botw():
+# both of these are going to alert me every hour if something pops up unfortunately
+# need to figure out a way to get dates maybe or the last title and see if it is different
 
+
+def nintendo():
+    #i know this is really similar to the grrm function but i believe the two will grow more different the more i develop
+    global holder
+    global counter
+    url = "http://www.nintendolife.com/news"
+    page = requests.get(url)
+    html = BeautifulSoup(page.content, 'html.parser')
+    nintendoLife = html.find_all('span', class_='title')
+
+    for title in nintendoLife:
+        words = title.text.lower()
+        # gotta figure out how to get the most recent one that is new. rn i can get new ones, but also old ones 
+        if(holder != words and counter == 0):
+            if (words.find('pikmin') != -1 or words.find('breath of the wild') != -1):
+                print(words)
+                holder = words
+                print(holder)
+                message = "Possible nintendo Update @ " + url
+                command = "curl -d \'{\"text\" : \"" + message + "\", \"bot_id\" : \"fa4d9cc813bdbd0f7d192054d4\"}\' https://api.groupme.com/v3/bots/post"
+                # os.system(command)
+                return
     return
+
 
 def grrm():
-        url = "https://www.georgerrmartin.com/notablog/"
-        page = requests.get(url)
-        html = BeautifulSoup(page.content, 'html.parser')
-        grrmPost = html.find('div', class_='post').text.lower().split()
-        command = "-1"
-        message = ""
+    url = "https://www.georgerrmartin.com/notablog/"
+    page = requests.get(url)
+    html = BeautifulSoup(page.content, 'html.parser')
+    grrmPost = html.find('div', class_='post').text.lower().split()
 
-        for word in grrmPost:
-            if (word == 'winds' or word == 'winter'):
-                message = "Possible Winds Update @ https://georgerrmartin.com/notablog/"
-                command = "curl -d \'{\"text\" : \"" + message + "\", \"bot_id\" : \"fa4d9cc813bdbd0f7d192054d4\"}\' https://api.groupme.com/v3/bots/post"
-
-        if(command != "-1"):
+    for word in grrmPost:
+        if (word == 'winds' or word == 'winter'):
+            message = "Possible Winds Update @ " + url
+            command = "curl -d \'{\"text\" : \"" + message + "\", \"bot_id\" : \"fa4d9cc813bdbd0f7d192054d4\"}\' https://api.groupme.com/v3/bots/post"
             os.system(command)
-        return
+            return
+    return
+
+
+# main()
+
 
 sched.start()
